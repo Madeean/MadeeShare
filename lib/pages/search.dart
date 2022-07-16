b5aab94e611ac66a323199af3f7edc1f43dd43ae
MadeReihan <reihanmade@gmail.com>
2022-07-16 23:16:04 +0700

    Image picker, post page

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,5 +1,10 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:social_network_flutter/models/user.dart';
+import 'package:social_network_flutter/pages/home.dart';
+import 'package:social_network_flutter/widgets/progress.dart';
 
 class Search extends StatefulWidget {
   @override
@@ -7,10 +12,27 @@ class Search extends StatefulWidget {
 }
 
 class _SearchState extends State<Search> {
+  Future<QuerySnapshot> searchResultsFuture;
+  TextEditingController searchController = TextEditingController();
+
+  handleSearch(String query) {
+    Future<QuerySnapshot> users = usersRef
+        .where("displayName", isGreaterThanOrEqualTo: query)
+        .getDocuments();
+    setState(() {
+      searchResultsFuture = users;
+    });
+  }
+
+  clearSearch() {
+    searchController.clear();
+  }
+
   AppBar buildSearchField() {
     return AppBar(
       backgroundColor: Colors.white,
       title: TextFormField(
+        controller: searchController,
         decoration: InputDecoration(
           hintText: "search for a user .. ",
           filled: true,
@@ -20,14 +42,16 @@ class _SearchState extends State<Search> {
           ),
           suffixIcon: IconButton(
             icon: Icon(Icons.clear),
-            onPressed: () => print('clear'),
+            onPressed: clearSearch,
           ),
         ),
+        onFieldSubmitted: handleSearch,
       ),
     );
   }
 
   Container buildNoContent() {
+    final orientation = MediaQuery.of(context).orientation;
     return Container(
       child: Center(
         child: ListView(
@@ -35,7 +59,7 @@ class _SearchState extends State<Search> {
           children: [
             SvgPicture.asset(
               'assets/images/search.svg',
-              height: 300,
+              height: orientation == Orientation.portrait ? 300 : 200,
             ),
             Text(
               'Find User',
@@ -53,19 +77,77 @@ class _SearchState extends State<Search> {
     );
   }
 
+  buildSearchResult() {
+    return FutureBuilder(
+      future: searchResultsFuture,
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return circularProgress();
+        }
+        List<UserResult> searchResults = [];
+        snapshot.data.documents.forEach((doc) {
+          User user = User.fromDocument(doc);
+          UserResult searchResult = UserResult(user);
+          searchResults.add(searchResult);
+        });
+
+        return ListView(
+          children: searchResults,
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor.withOpacity(0.8),
       appBar: buildSearchField(),
-      body: buildNoContent(),
+      body:
+          searchResultsFuture == null ? buildNoContent() : buildSearchResult(),
     );
   }
 }
 
 class UserResult extends StatelessWidget {
+  final User user;
+
+  UserResult(this.user);
+
   @override
   Widget build(BuildContext context) {
-    return Text("User Result");
+    return Container(
+      color: Theme.of(context).primaryColor.withOpacity(0.7),
+      child: Column(
+        children: [
+          GestureDetector(
+            onTap: () => print("tapped"),
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.grey,
+                backgroundImage: CachedNetworkImageProvider(user.photoUrl),
+              ),
+              title: Text(
+                user.displayName,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              subtitle: Text(
+                user.username,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          Divider(
+            height: 2.0,
+            color: Colors.white54,
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/pages/upload.dart
+++ lib/pages/upload.dart
@@ -1,13 +1,229 @@
+import 'dart:io';
+
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:social_network_flutter/models/user.dart';
 
 class Upload extends StatefulWidget {
+  final User currentUser;
+  Upload({this.currentUser});
+
   @override
   _UploadState createState() => _UploadState();
 }
 
 class _UploadState extends State<Upload> {
+  PickedFile selectFile;
+  File file;
+  final ImagePicker pick = ImagePicker();
+
+  handleTakePhoto() async {
+    Navigator.pop(context);
+    PickedFile selectFile = (await pick.getImage(
+      source: ImageSource.camera,
+      maxHeight: 675,
+      maxWidth: 960,
+    ));
+    setState(() {
+      this.file = File(selectFile.path);
+      print(this.file);
+    });
+  }
+
+  handleChooseFromGallery() async {
+    Navigator.pop(context);
+    PickedFile selectFile = await pick.getImage(source: ImageSource.gallery);
+    setState(() {
+      this.file = File(selectFile.path);
+      print(this.file);
+    });
+  }
+
+  selectImage(parentContext) {
+    return showDialog(
+      context: parentContext,
+      builder: (context) {
+        return SimpleDialog(
+          title: Text("create post"),
+          children: [
+            SimpleDialogOption(
+              child: Text('photo with camera'),
+              onPressed: handleTakePhoto,
+            ),
+            SimpleDialogOption(
+              child: Text('photo from gallery'),
+              onPressed: handleChooseFromGallery,
+            ),
+            SimpleDialogOption(
+              child: Text('cancel'),
+              onPressed: () => Navigator.pop(context),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Container buildSplashScreen() {
+    return Container(
+      color: Theme.of(context).colorScheme.secondary.withOpacity(0.6),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SvgPicture.asset(
+            'assets/images/upload.svg',
+            height: 260,
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 20),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                backgroundColor: Colors.deepOrange,
+              ),
+              onPressed: () => selectImage(context),
+              child: Text(
+                'Upload image',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  clearImage() {
+    setState(() {
+      file = null;
+    });
+  }
+
+  Scaffold buildUplaodedForm() {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white70,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
+          onPressed: clearImage,
+        ),
+        centerTitle: true,
+        title: Text(
+          'Caption Post',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        actions: [
+          Container(
+            margin: EdgeInsets.only(top: 16, right: 20),
+            child: GestureDetector(
+              onTap: () => print('pressed'),
+              child: Text(
+                'Post',
+                style: TextStyle(
+                    color: Colors.blueAccent,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: ListView(
+        children: [
+          Container(
+            height: 220,
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: Center(
+              child: AspectRatio(
+                aspectRatio: 16 / 9,
+                child: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: FileImage(file),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 10.0),
+          ),
+          ListTile(
+            leading: CircleAvatar(
+              backgroundImage: CachedNetworkImageProvider(
+                widget.currentUser.photoUrl,
+              ),
+            ),
+            title: Container(
+              width: 250,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Write a caption",
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(
+              Icons.pin_drop,
+              color: Colors.orange,
+              size: 35,
+            ),
+            title: Container(
+              width: 250,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Where was this photo taken",
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            width: 200,
+            height: 100,
+            alignment: Alignment.center,
+            child: TextButton.icon(
+              onPressed: () => print('get loc'),
+              icon: Icon(
+                Icons.my_location,
+                color: Colors.white,
+              ),
+              label: Text(
+                'Use Current Location',
+                style: TextStyle(color: Colors.white),
+              ),
+              style: TextButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                backgroundColor: Colors.blue,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text("Upload");
+    return file == null ? buildSplashScreen() : buildUplaodedForm();
   }
 }
